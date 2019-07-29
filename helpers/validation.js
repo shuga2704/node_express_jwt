f1e811dde0343121b531644207eb19125a97f49b
shuga2704 <shuga2704@gmail.com>
2019-07-29 03:39:43 +0300

    Initial commit

---
+++ helpers/validation.js
@@ -0,0 +1,22 @@
+const Joi = require('@hapi/joi');
+
+const registerValidation = (data) => {
+  const schema = {
+    name: Joi.string().min(5).required(),
+    email: Joi.string().min(5).required().email(),
+    password: Joi.string().min(5).required()
+  };
+  return Joi.validate(data, schema);
+}
+
+const loginValidation = (data) => {
+  const schema = {
+    email: Joi.string().min(5).required().email(),
+    password: Joi.string().min(5).required()
+  };
+  return Joi.validate(data, schema);
+}
+
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
